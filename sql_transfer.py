dc55b057a0b987a2c5fd256bc0efbc87695d2436
Elias Cassis <ecassis@wisc.edu>
2024-05-31 16:08:03 -0500

    add for moving of settled overdues

--- sql_transfer.py
+++ sql_transfer.py
@@ -22,7 +22,7 @@ db.run("ALTER TABLE excluded_allocations ADD COLUMN processed TIMESTAMP")
 
 db.run("CREATE EXTENSION IF NOT EXISTS intarray") # required for subracting array of ints from array of ints
 db.run("CREATE TABLE IF NOT EXISTS overdues (patron_oid INTEGER PRIMARY KEY, count INTEGER DEFAULT 0, hold_count INTEGER DEFAULT 0, fee_count INTEGER DEFAULT 0, hold_length INTERVAL DEFAULT CAST('0' AS INTERVAL), hold_remove_time TIMESTAMP, invoice_oids INTEGER[], registrar_hold_count INTEGER DEFAULT 0)")
-db.run("CREATE TABLE IF NOT EXISTS invoices (invoice_oid INTEGER PRIMARY KEY, count INTEGER DEFAULT 0, hold_status BOOLEAN DEFAULT FALSE, fee_status BOOLEAN DEFAULT FALSE, registrar_hold BOOLEAN DEFAULT FALSE, hold_length INTERVAL DEFAULT CAST('0' AS INTERVAL), hold_remove_time TIMESTAMP, ck_oid INTEGER, patron_oid INTEGER, waived BOOLEAN DEFAULT FALSE, expiration TIMESTAMP)")
+db.run("CREATE TABLE IF NOT EXISTS invoices (invoice_oid INTEGER PRIMARY KEY, count INTEGER DEFAULT 0, hold_status BOOLEAN DEFAULT FALSE, fee_status BOOLEAN DEFAULT FALSE, registrar_hold BOOLEAN DEFAULT FALSE, hold_length INTERVAL DEFAULT CAST('0' AS INTERVAL), overdue_start_time TIMESTAMP, hold_remove_time TIMESTAMP, ck_oid INTEGER, patron_oid INTEGER, waived BOOLEAN DEFAULT FALSE, expiration TIMESTAMP overdue_lost BOOLEAN DEFAULT FALSE)")
 
 for patron_oid, count, hold_status, fee_status, _, hold_remove_time, invoice_oid, registrar_hold in db.all("SELECT * FROM overdues_old"):
     allocs = wco.get_patron_checkouts(patron_oid, ['scheduledEndTime', 'realEndTime']).json()['payload']['result']
@@ -45,7 +45,10 @@ for patron_oid, count, hold_status, fee_status, _, hold_remove_time, invoice_oid
         waived = False
         exp = None
     else:
-        pass
+        # use arbitrary previous invoice with specific expiration date & waived
+        invoice_oid = wco.get_patron(patron_oid, ['invoices']).json()['payload']['invoices'][-1]['oid']
+        waived = True
+        exp = datetime(year=2024, month=6, day=1) + timedelta(days=365*4)
 
     db.run("INSERT INTO overdues VALUES (%(p_oid)s, %(count)s, %(hold_count)s, %(fee_count)s, %(hold_len)s, %(hold_rtime)s, %(i_id)s, %(reg_hold_count)s)",
             p_oid = patron_oid,
@@ -56,13 +59,14 @@ for patron_oid, count, hold_status, fee_status, _, hold_remove_time, invoice_oid
             hold_rtime = hold_rtime,
             i_id = i_id,
             reg_hold_count = int(registrar_hold))
-    db.run("INSERT INTO invoices VALUES (%(i_id)s, %(count)s, %(h_status)s, %(f_status)s, %(reg_hold)s, %(hold_len)s, %(hold_rtime)s, %(ck_id)s, %(p_id)s, %(waived)s, %(exp)s)",
+    db.run("INSERT INTO invoices VALUES (%(i_id)s, %(count)s, %(h_status)s, %(f_status)s, %(reg_hold)s, %(hold_len)s, %(o_stime)s, %(hold_rtime)s, %(ck_id)s, %(p_id)s, %(waived)s, %(exp)s)",
             i_id = invoice_oid,
             count = count,
             h_status = hold_status,
             f_status = fee_status,
             reg_hold = registrar_hold,
             hold_len = hold_len,
+            o_stime = f"{datetime.now()}::TIMESTAMP",
             hold_rtime = hold_remove_time,
             ck_id = lastest_overdue,
             p_id = patron_oid,
